453c0efea36bf0e8ff6f02fef13301ffd6c79371
Nickolas Stricker <43568886+NickStrick@users.noreply.github.com>
2019-07-25 22:10:36 -0500

    added user seeds

---
+++ data/seeds/001-user.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcryptjs');
+
+exports.seed = function (knex, Promise) {
+  const users = [
+    { username: 'admin', password: bcrypt.hashSync('123', 10), email: 'raj@example.com' },
+    { username: 'a', password: bcrypt.hashSync('123', 10), email: 'manju@example.com' },
+    { username: 'b', password: bcrypt.hashSync('123', 10), email: 'blabla@example.com' }
+  ]
+
+  return (
+    knex
+      // Deletes ALL existing entries for users table
+      .raw("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
+      .then(function () {
+        return knex("users").insert(users);
+      })
+  );
+};
